10abe4da16443a6c2459c8db90a7005f30f7b564
m4549071758 <m4549071758@gmail.com>
2025-04-15 14:38:03 +0900

    Update main.py

--- main.py
+++ main.py
@@ -14,28 +14,40 @@ async def on_ready():
 @client.event
 async def on_message(message):
     if message.author == client.user:
-        return #自分自身のアクションは無視
-    
-    if client.user.mentioned_in(message): #メンションされたら
-        old_name = message.author.display_name #ユーザー名の取得
+        return
 
+    if client.user.mentioned_in(message):
         await message.channel.send("契約書だよ。そこに名前を書きな。")
         await asyncio.sleep(1)
-        await message.channel.send(f"あなたは契約書に`{old_name}`と書き込んだ。")
+        await message.channel.send(f"(あなたは契約書に`{message.author.display_name}`と書き込んだ。)")
 
-        await asyncio.sleep(1)
-        await message.channel.send(f"フン。`{old_name}`というのかい。贅沢な名だね。")
+        name = message.author.display_name
+        await asyncio.sleep(2)
+        length = len(name)
+        if length == 1:
+            hiragana = [chr(code) for code in range(0x3041, 0x3097)]
+            katakana = [chr(code) for code in range(0x30A1, 0x30FA)]
+            kana = hiragana + katakana
+            new_name = "".join(random.choices(kana, k=12))
+            await message.channel.send(f"フン。`{name}`というのかい。短い名前だねぇ。")
+            await asyncio.sleep(2)
+            await message.channel.send(f"短い名前は好きじゃないんだ。")
+            await asyncio.sleep(2)
+            await message.channel.send(f"今からお前の名前は`{new_name}`だ。")
+            await asyncio.sleep(2)
+            await message.channel.send(f"いいかい、`{new_name}`だよ。分かったら返事をするんだ、`{new_name}`!!")
+        
+        else:
+            await message.channel.send(f"フン。`{name}`というのかい。贅沢な名だねぇ。")
+            name_length = random.randint(1,10)
+            new_name_listed = random.choices(name.replace(" ",""), k=name_length)
+            new_name = "".join(new_name_listed)
+            await asyncio.sleep(4)
+            await message.channel.send(f"今からお前の名前は`{new_name}`だ。"
+            f"いいかい、`{new_name}`だよ。分かったら返事をするんだ、`{new_name}`!!")
 
-        name_length = random.randint(1,len(old_name))
-        new_name_listed = random.choices(name.replace(" ",""), k=name_length) #空白は潰し、randomでchoice
-        new_name = "".join(new_name_listed) #リストから文字列へ
+        await message.author.edit(nick=new_name)
 
-        await asyncio.sleep(3)
-        await message.channel.send(f"今からお前の名前は`{new_name}`だ。"
-        f"いいかい、`{new_name}`だよ。分かったら返事をするんだ、`{new_name}`!!")
-
-        await message.author.edit(nick=new_name) #ニックネームを変更
-
-        await message.channel.send(f"あなたのニックネームは`{new_name}`になった。")
+        await message.channel.send(f"(あなたのニックネームは`{new_name}`になった。)")
 
 client.run("YOUR_TOKEN_HERE")
